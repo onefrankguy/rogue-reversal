205df6580353ccfff329434d5f02fc7eaa54ca28
Frank Mitchell <me@frankmitchell.org>
2015-09-11 11:18:35 -0400

    Provides a more rogue like hero and scales up the image using canvas.

--- js/game.js
+++ js/game.js
@@ -335,27 +335,12 @@ var ImageCache = (function () {
 
 var $ = window.jQuery
   , canvas = $('#gfx').unwrap()
-  , ctx = null
-  , images = {}
-  , ic = {}
+  , ctx = canvas.getContext('2d')
+  , images = Object.create(null)
+  , ic = Object.create(null)
 
-if (canvas) {
-  ctx = canvas.getContext('2d')
-}
 images['mannequin'] = {"width":16,"height":24,"data":"67a2b7a2b4a2b1c1b5a1b2c1b3a1b3c5b1d2c1b3a1b3c6d2c1b3a1b1c1b1c2d2e2d1b1c1b3a1b1c1b1c1d2e3d1b1c1b3a1b1c1b1c1d2e2d1e1b1c1b3a1b1c1b2c1d1e2d1e1b1c1b4a2b3c3d1e2b6a1b6c1e1b7a1f7b1f7a1f7g1f7a1f2g1h4g1f7a1f1g1h1f1a1f1h1g1f7a1f1g1h1f1a1f1h1g1f7a1f1g1h1f1a1f1h1g1f7a1f1g1h1f1a1f1h2f7a1i2g1i1a1f1h2f6a2i2f1i1a2i1f2i5a5i1a5i2a","values":{"a":"#00000000","b":"#afafafff","c":"#d0d0d0ff","d":"#ecececff","e":"#ffffffff","f":"#414141ff","g":"#606060ff","h":"#848484ff","i":"#1b1b1bff"}}
-
-/*
- * mannequin
- * a = background
- * b = shirt outline
- * c = shirt base
- * d = shirt highlight 1
- * e = shirt highlight 2
- * f = pants outline
- * g = pants base
- * h = pants highlight
- * i = shoes
- */
+images['hero'] = {"width":32,"height":18,"data":"38a4b12a4b11a1b4c1b10a1b1c2d1c1b9a1b6c1b8a1b1c4d1c1b8a1b1c4d1c1b8a1b1c4d1c1b8a3b2d3b8a1b2c2d2c1b8a1b1e1f2d1f1e1b8a1b6c1b7a1g1a1b1c2d1c1b1a1g8a1b4c1b7a2h1g1h1b2c1b1h1g2h4a2h1g1h4i1h1g2h3a2g1h1i1g1h2i1h1g1i1h2g2a2g2h1g1h2i1h1g2h2g4a1g1h2g2h2g1h1g6a1g1h2g2h2g1h1g5a1g1h2g1i2g1i2g1h1g4a1g1h2g1h2g1h2g1h1g3a1b1d1c1g1h1g2h1g1h1g1c1d1b2a1b1d1c1g1h1g2h1g1h1g1c1d1b2a1b2d1c1g1h2g1h1g1c2d1b2a1b2c2g1h2g1h2g2c1b3a2b1a1g1i2h1i1g1a2b4a2b2g1h2i1h2g2b7a1g4h1g10a1g1h2g1h1g9a1g1i1h2g1h1i1g8a1g2h2g2h1g36a","values":{"a":"#00000000","b":"#391e13ff","c":"#d48150ff","d":"#f3cc93ff","e":"#fefefeff","f":"#252a4fff","g":"#0f0c0fff","h":"#343434ff","i":"#6d6d6dff"}}
 
 function hexToRgba (hex) {
   var result = /^#([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex)
@@ -367,9 +352,9 @@ function hexToRgba (hex) {
   } : null
 }
 
-ic.getDataURL = function (name, options) {
+ic.getDataURL = function (name) {
   var data = images[name]
-    , image = ctx.createImageData(data.width, data.height)
+    , image = ctx.createImageData(data.width * 2, data.height * 2)
     , i = 0
     , j = 0
     , x = 0
@@ -379,41 +364,8 @@ ic.getDataURL = function (name, options) {
     , value = ''
     , number = ''
 
-  options = options || {}
-  if (options.hasOwnProperty('shirt')) {
-    color = options['shirt']
-    switch (name) {
-      case 'mannequin':
-        data.values.b = ColorWheel.shade(color)
-        data.values.c = ColorWheel.tone(color)
-        data.values.d = ColorWheel.hue(color)
-        data.values.e = ColorWheel.tint(color)
-        break
-    }
-  }
-
-  if (options.hasOwnProperty('pants')) {
-    color = options['pants']
-    switch (name) {
-      case 'mannequin':
-        data.values.f = ColorWheel.shade(color)
-        data.values.g = ColorWheel.tone(color)
-        data.values.h = ColorWheel.hue(color)
-        break
-    }
-  }
-
-  if (options.hasOwnProperty('shoes')) {
-    color = options['shoes']
-    switch (name) {
-      case 'mannequin':
-        data.values.i = ColorWheel.hue(color)
-        break
-    }
-  }
-
-  canvas.width = data.width
-  canvas.height = data.height
+  canvas.width = data.width * 2
+  canvas.height = data.height * 2
 
   for (i = 0; i < data.data.length; i += 1) {
     value = data.data[i]
@@ -421,15 +373,34 @@ ic.getDataURL = function (name, options) {
       number = parseInt(number, 10)
       color = hexToRgba(data.values[value])
       for (j = 0; j < number; j += 1) {
-        offset = (y*(image.width*4)) + (x*4)
+        offset = ((y+0)*(image.width*4)) + ((x+0)*4)
         image.data[offset + 0] = color.r
         image.data[offset + 1] = color.g
         image.data[offset + 2] = color.b
         image.data[offset + 3] = color.a
-        x += 1
+
+        offset = ((y+0)*(image.width*4)) + ((x+1)*4)
+        image.data[offset + 0] = color.r
+        image.data[offset + 1] = color.g
+        image.data[offset + 2] = color.b
+        image.data[offset + 3] = color.a
+
+        offset = ((y+1)*(image.width*4)) + ((x+0)*4)
+        image.data[offset + 0] = color.r
+        image.data[offset + 1] = color.g
+        image.data[offset + 2] = color.b
+        image.data[offset + 3] = color.a
+
+        offset = ((y+1)*(image.width*4)) + ((x+1)*4)
+        image.data[offset + 0] = color.r
+        image.data[offset + 1] = color.g
+        image.data[offset + 2] = color.b
+        image.data[offset + 3] = color.a
+
+        x += 2
         if (x >= image.width) {
           x = 0
-          y += 1
+          y += 2
         }
         if (y >= image.height) {
           y = 0
@@ -1591,6 +1562,8 @@ Game.play = function () {
   }
 
   $('#items').html(html)
+
+  $('#hero-fx').unwrap().style.backgroundImage = 'url('+ImageCache.getDataURL('hero')+')'
 
   for (i = 0; i < items.length; i += 1) {
     $('#'+items[i]).touch(onItem)
